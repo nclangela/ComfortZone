5278b8d69f24ac7ead291b5d757dbcf8d5eb1141
nclangela <nclangela.rvr@gmail.com>
2013-04-30 17:25:32 +0800

    first commit

---
+++ comfortzone/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Comfortzone::Application.config.secret_token = '033d1f20c7c75b8e5cec291c35709a2194f10fcbaa827b660ae432945e59541fc5bfc49aafbef886c89a4221ce3d3a23838aa941090ef94c345aa3d5086aa570'
